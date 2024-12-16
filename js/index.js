1c7793ccd3aa29c198f7f19f450412e0750790a1
Tracie Childers <traciechilders01@gmail.com>
2024-12-16 09:44:38 -0500

    edited README.md completed js/index.js

--- js/index.js
+++ js/index.js
@@ -2,3 +2,29 @@
 // that are not declared
 "use strict"
 
+/*
+const hairColor = "black";
+console.log(hairColor); //Output "black"
+console.log(type of hairColor); // "string"
+
+const ten = 10;
+console.log(ten); //Output 10
+console.log(typeof ten);  //Output number
+
+
+let isStudent = true;
+console.log(isStudent);  //Output true
+console.log(typeof isStudent); //Output boolean// 
+
+let didGit = false;
+console.log(didGit); //Output false//
+console.log(typeof didGit); //boolean//
+
+let didGit = "Finally";  
+console.log(didGit); //Output False// Uncaught Syntex Error:  redeclaration of let didGit//
+console.log(typeof didGit); //Output boolean//
+
+digGit = "Finally";
+console.log(didGit); //Output "Finally"//
+console.log(typeof didGit); //Output string//
+ */
