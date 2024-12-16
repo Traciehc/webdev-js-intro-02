c1fdf124ac754fa83a45eb9751fa2d0b0380e820
Tracie Childers <traciechilders01@gmail.com>
2024-12-16 10:29:01 -0500

    Updated README.md completed js/assignment.js

--- js/assignment.js
+++ js/assignment.js
@@ -2,6 +2,8 @@
 
 // Declaring constant variables to store references to the elements
 // that will be updated by your code.
+
+
 const yearElement = document.getElementById("year");
 const resolutionElement = document.getElementById("resolution")
 const willMeetResolutionElement = document.getElementById("will-meet-resolution")
@@ -9,27 +11,47 @@ const submissionBtn = document.getElementById("submission-btn")
 
 // Declare your variables here.
 
+const currentYear = new Date().getFullYear();
+const resolution = "Continue Learning JavaScript";
+let willMeetResolution = true; 
+
 function updateYear() {
     // Update this function
 
+    yearElement.textContent = currentYear;
 }
 
 function updateResolution() {
     // Update this function 
+
+    resolutionElement.textContent = resolution;
+
 
 }
 
 function updateWillMeetResolution() {
     // Update this function
 
+    willMeetResolutionElement.textContent = willMeetResolution ? "Yes" : "No";
+
 }
 
 function render() {
     // Update this function
+
+    updateYear();
+    updateResolution();
+    updateWillMeetResolution();
+
 
 }
 
 submissionBtn.addEventListener("click", function () {
     // Update this function
 
-})
+    willMeetResolution = !willMeetResolution; 
+    render();
+
+});
+
+render();
